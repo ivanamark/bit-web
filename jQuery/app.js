9e2cec7eee8052cb985aa5247b37edf2b916c2aa
Ivana Markovic <ivana.markovic69@gmail.com>
2018-03-07 02:00:52 +0100

    jquery tasks

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,17 +1,17 @@
 var links = [
-    "http://via.placeholder.com/350x150",
-    "http://via.placeholder.com/350x150",
-    "http://via.placeholder.com/350x150",
-    "http://via.placeholder.com/350x150",
-    "http://via.placeholder.com/350x150",
-    "http://via.placeholder.com/350x150"
+    "http://via.placeholder.com/300x200/cc3300/000000",
+    "http://via.placeholder.com/300x200/0E2F44/000000",
+    "http://via.placeholder.com/300x200/ffcc00/000000",
+    "http://via.placeholder.com/300x200/009933/000000",
+    "http://via.placeholder.com/300x200/cc3300/000000",
+    "http://via.placeholder.com/300x200/009933/000000"
 ]
 
 var $gallery = $('.gallery');
 $body = $("body");
 
 $body.prepend('<h1>Amazing galery</h1>');
-
+$body.css("background-color","#ffffcc");
 for (var i = 0; i < links.length; i++) {
     var element = links[i];
     var img = $("<img>");
@@ -20,9 +20,8 @@ for (var i = 0; i < links.length; i++) {
 }
 
 $("img").each(function (index, element) {
-    var random = Math.random() * (500 - 50) +50;
-    
+    var random = Math.random() * (300 - 50) + 50;
     $(element).css("width", random);
-    var random = Math.random() * (100 - 10) +10;
+    var random = Math.random() * (30 - 5) + 5;
     $(element).css("padding", random);
 });
